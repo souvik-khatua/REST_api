46792b7ad45ef0691f5e078fdb3b3fad46fdbe5b
bisu <khatuasouvik647@gmail.com>
2025-04-06 19:46:25 +0530

    file saving

---
+++ fruits/index.js
@@ -0,0 +1,5 @@
+const apple =require("./apple");
+const banana =require("./banan");
+const orange =require("./orange");
+let fruits=[apple,banana,orange];
+module.exports = fruits;
---
+++ jnsk.js
@@ -0,0 +1,4 @@
+import {sum, pi} from "./math.js";
+// console.log(sum(1,2));
+import {generate} from "random-words";
+console.log(generate());
